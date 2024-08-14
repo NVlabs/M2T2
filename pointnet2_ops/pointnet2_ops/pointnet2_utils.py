8c1a3883ec9fa64721975f8fc6a59c80cfbb0b46
Arhan Jain <arhan.j04@gmail.com>
2024-08-14 16:15:15 -0700

    hardcoded compilation, undefined function in agent

--- pointnet2_ops/pointnet2_ops/pointnet2_utils.py
+++ pointnet2_ops/pointnet2_ops/pointnet2_utils.py
@@ -20,7 +20,7 @@ except ImportError:
     )
     _ext_headers = glob.glob(osp.join(_ext_src_root, "include", "*"))
 
-    os.environ["TORCH_CUDA_ARCH_LIST"] = "3.7+PTX;5.0;6.0;6.1;6.2;7.0;7.5"
+    # os.environ["TORCH_CUDA_ARCH_LIST"] = "3.7+PTX;5.0;6.0;6.1;6.2;7.0;7.5"
     _ext = load(
         "_ext",
         sources=_ext_sources,
